1a2cc81ce7bd79204988267fd964f7d8bdaa799d
vini2003 <vini.sch@outlook.com>
2024-03-20 17:31:41 -0300

    Extremely minor refactor.

--- source/main.cpp
+++ source/main.cpp
@@ -1,9 +1,8 @@
 #include "application.h"
 
 int main() {
-	vox::Application application;
-
 	try {
+		vox::Application application;
 		application.run();
 	} catch (const std::exception& exception) {
 		std::cerr << exception.what() << std::endl;
